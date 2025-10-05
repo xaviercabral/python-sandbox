04bb3c663c118b7c18646646dc9566c8e7f07571
xaviercabral <xaviercabral303@gmail.com>
2025-10-05 18:55:35 -0400

    feat: operators

--- basics/calculator.py
+++ basics/calculator.py
@@ -1,3 +1,21 @@
 a = int(input("enter first number: "))
 b = int(input("enter second number: "))
-print("The sum is: ", a+b)
+
+problem_solve = input("what is the problem you wanna do?(addition, subtraction, multiplication, division): ")
+
+if problem_solve == "addition":
+    print("The sum is: ", a+b)
+elif problem_solve == "subtraction":
+    print("The subtraction is: ", a-b)
+elif problem_solve == "multiplication":
+    print("The multiplication is: ", a*b)
+elif problem_solve == "division":
+    if b==0:
+        print("Infinite Number")
+    else:    
+          print("The division is: ", a/b)
+else:
+     print("Insert one of the options")
+
+
+    
